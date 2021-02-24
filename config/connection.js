773e994d44f456d04c1c77f3defd675f4bc6dd0e
Brie Bencich <bbencich@hotmail.com>
2021-02-24 19:04:44 +0200

    updating

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose'); 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost.socialnetworkapi', { 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost.social-network-api-', { 
     useNewURLParser: true, 
     useUnifiedTopology: true, 
     useCreateIndex: true, 
